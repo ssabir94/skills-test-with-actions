9b36b520383b8bd679aef75b592ff213ae5cc605
ssabir94 <sanasabir94@gmail.com>
2026-09-25 13:13:37 +0200

    Refactor comments in Fibonacci test case

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -60,11 +60,11 @@ def test_get_nth_fibonacci_one():
 
  def test_get_nth_fibonacci_ten():
       """Test with n=10."""
-#     # Arrange
-      n = 10
+    # Arrange
+    n = 10
 
-#     # Act
-      result = get_nth_fibonacci(n)
+    # Act
+    result = get_nth_fibonacci(n)
 
-#     # Assert
-      assert result == 55
+    # Assert
+    assert result == 55
